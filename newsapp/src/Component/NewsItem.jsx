93994a3a5216d0aaa17f05649792fc7cfe120f51
pariraj18 <pariraj18092006@gmal.com>
2026-07-09 09:01:50 +0530

    Fetching API key from newsAPI

--- newsapp/src/Component/NewsItem.jsx
+++ newsapp/src/Component/NewsItem.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
-export class NewsItem extends Component {
+export class
+ NewsItem extends Component {
   render() {
+    let { title, description,imageurl,newsurl} = this.props;
     return (
-      <div>
-        
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem;"}}>
+  <img src={imageurl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href={newsurl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
+  </div>
+</div>
       </div>
     )
   }
